40957e544f9b1bb0ac87952860ce7a7932c2a5ba
hust-szw <1005204767@qq.com>
2021-08-21 20:19:34 +0800

    Add files via upload

--- qqbot.py
+++ qqbot.py
@@ -7,50 +7,134 @@ import time
 import requests
 import json
 from bs4 import BeautifulSoup
+from demjson import *
 #这里定义接收到消息所用方法
 @itchat.msg_register(TEXT)
 def msg_reply(msg):
-    print(msg)
+    remark_name = decode(str(msg).replace("<","{").replace(">","}"))['User']['User']['RemarkName']
     info = msg['Text']
     global state
     if (state==0):
         if (info == "雨泽"):
             menu = "你好呀，我是雨泽小朋友，请问喊我有什么事？目前爸爸妈妈只教会了我以下技能哦~\n" \
                    "(你可以选择前面的序号让我来给你展示~)\n" \
-                   "0：雨泽成长记录\n" \
-                   "1：研招网消息查看\n"
+                   "1：雨泽成长记录\n" \
+                   "2：研招网消息查看\n"\
+                   "3: 艾宾浩斯\n"
             state = 1
             return menu
+        elif (info == "艾宾浩斯"):
+            msg = "功能选择：\n" \
+                  "1：输入今日学习任务\n" \
+                  "2：查看近五日学习任务\n" \
+                  "3：删除学习任务\n"
+            state = 3
+            return msg
+
         else:
             return get_response(info.encode('UTF-8'))
     if(state == 1):
-        if(info =="0"):
+        if(info =="1"):
             jieshao = "你好，我的名字叫邵雨泽~雨亭的雨，泽为的泽~你可以喊我雨泽！\n\n" \
                       "7月17日，我被爸爸妈妈带到了这个世界，好开心~\n\n" \
                       "7月24日，我学会了给爸爸妈妈汇报每日的天气情况，成为了爸爸妈妈的贴心小助手~\n\n" \
-                      "7月27日，爸爸教会了我说话，他说这样他不在的时候我就可以陪着妈妈~\n\n"
+                      "7月27日，爸爸教会了我说话，他说这样他不在的时候我就可以陪着妈妈~\n\n" \
+                      "8月13日，我可以帮爸爸妈妈查询和推送他们目标学校的考研信息啦~\n\n" \
+                      "8月21日，我可以提醒爸爸妈妈用艾宾浩斯记忆曲线复习学习内容了~\n\n"
             state = 0
             return jieshao
-        elif(info == "1"):
+        elif(info == "2"):
             msg = "请选择查看的学院：\n" \
-                  "1：西安交通大学计算机学院\n" \
-                  "2：西北工业大学软件学院\n"
+                  "1：西北工业大学软件学院\n" \
+                  "2：西安交通大学计算机学院\n"
             state = 2
+            return msg
+        elif (info == "3"):
+            msg = "功能选择：\n" \
+                  "1：输入今日学习任务\n" \
+                  "2：查看近五日学习任务\n" \
+                  "3：删除学习任务\n"
+            state = 3
             return msg
         else:
             state = 0
             return("抱歉呦，雨泽还不会这项技能呢~")
     if (state == 2):
-        if(info == "1"):
+        if(info == "2"):
             return_msg = ""
             xjd_msg = get_xjd_msg()
             for i in range(10):
                 return_msg =return_msg+xjd_msg[i]+"\n"
             state = 0
             return return_msg
-        elif(info == "2"):
+        elif(info == "1"):
+            return_msg = "招生公告信息：\n"
+            xgd_msg1 = get_xgd_msg(1)
+            xgd_msg2 = get_xgd_msg(2)
+            for i in range(5):
+                return_msg =return_msg+xgd_msg1[i]+"\n"
+            return_msg = return_msg+"\n招生简章信息：\n"
+            for i in range(5):
+                return_msg = return_msg + xgd_msg2[i] + "\n"
+            state = 0
+            return return_msg
+    if(state == 3):
+        if(info == "1"):
+            msg = "请输入学习任务："
+            state = 4
+            return msg
+        elif(info == '2'):
+            msg = "近五日学习任务如下：\n"
+            for e in msgbox:
+                if ((e["day"] < 5) & (remark_name == e["nickname"])):
+                    msg = msg + str(e["day"]) + "天前：\n" + e["msg"] + "\n\n"
+            state = 0
+            return msg
+        elif (info == '3'):
+            msg = "学习任务如下："
+            i = 0
+            for e in msgbox:
+                if ( remark_name == e["nickname"]):
+
+                    msg = msg + str(i)+".\n"+str(e["day"]) + "天前：\n" + e["msg"] + "\n\n"
+                    i= i+1
+            state = 0
+            msg= msg+"请输入需要删除的任务编号"
+            state = 6
+            return msg
+        else:
             pass
+    if (state == 4): #创建学习任务
+        creatmsg(info,remark_name)
         state = 0
+        return ("今日任务创建成功")
+    if(state == 5): #查看学习任务
+        msg = ""
+        i = 1
+        for e in msgbox:
+            if ((e["day"] < 5) & (remark_name == e["nickname"])):
+                msg = msg + str(e["day"]) + "天前：\n" + e["msg"] + "\n\n"
+        state = 0
+        return msg
+    if (state == 6): #删除学习任务
+        try:
+            num = int(info)
+        except:
+            state = 0
+            return "输入数字非法"
+        if(num<0 or num>len(msgbox)-1):
+            state = 0
+            return "输入数字非法"
+        else :
+            try:
+                msgbox.pop(num)
+            except:
+                state = 0
+                return "删除失败"
+            with open("msg.txt", "w+") as f:
+                f.write(encode(msgbox))
+            state = 0
+            return "删除成功"
 
 #获得交大研招网信息
 def get_xjd():
@@ -67,6 +151,50 @@ def get_xjd():
     # xjd_last = list
     return list
 
+#艾宾浩斯提醒
+def creatmsg(msg,nickname):
+    global msgbox
+    m = {
+        "msg":msg,
+        "nickname":nickname,
+        "day":0
+    }
+    msgbox.append(m)
+    with open("msg.txt","w+") as f:
+        f.write(encode(msgbox))
+
+def msg_daily_push():
+    with open("msg.txt", "r+") as f:
+        temp_msg = decode(f.read())
+        for people in push_list:
+            target = ""
+            for msg in temp_msg:
+                print(msg)
+                if(msg['nickname']==people['remark_name']):
+                    msg["day"]=msg["day"]+1
+                    if(msg["day"]==1):
+                        target = target + "\n第1次复习：\n"+msg['msg']+"\n"
+                    elif(msg["day"]==3):
+                        target = target + "\n第2次复习：\n" + msg['msg']+"\n"
+                    elif (msg["day"] == 7):
+                        target = target + "\n第3次复习：\n" + msg['msg'] + "\n"
+                    elif (msg["day"] == 14):
+                        target = target + "\n第4次复习：\n" + msg['msg'] + "\n"
+                    elif (msg["day"] == 29):
+                        target = target + "\n第5次复习：\n" + msg['msg'] + "\n"
+                    elif(msg["day"] > 29):
+                        temp_msg.remove(msg)
+            if(target == ""):
+                user_name = itchat.search_friends(remarkName=people["remark_name"])[0]["UserName"]
+                itchat.send_msg("今日暂无学习任务", toUserName=user_name)
+            else:
+                user_name = itchat.search_friends(remarkName=people["remark_name"])[0]["UserName"]
+                itchat.send_msg("今日学习任务如下：\n"+target, toUserName=user_name)
+        with open("msg.txt", "w+") as f:
+            f.write(encode(temp_msg))
+        global msgbox
+        msgbox = temp_msg
+        print(temp_msg)
 
 #交大资讯更新推送
 def push_xjd():
@@ -90,6 +218,12 @@ def push_xjd():
         xjd_last = list((set(xjd_last)-set(old_msg))|set(new_msg))
     return push_msg
 
+def xjd_daily_push():
+    pushlist  = push_xjd()
+    if(pushlist!=[]):
+        for people in push_list:
+            user_name = itchat.search_friends(remarkName=people["remark_name"])[0]["UserName"]
+            itchat.send_msg(pushlist, toUserName=user_name)
 def get_xjd_msg():
     i= 1
     msg_list = []
@@ -108,6 +242,98 @@ def get_xjd_msg():
         msg_list.append(push_msg)
         i=i+1
     return msg_list
+
+#西工大消息推送
+def get_xgd(index):
+    if(index == 1): #招生公告
+        url = "http://yzb.nwpu.edu.cn/new/sszs/zsgg.htm"
+    elif(index == 2): #招生简章
+        url = "http://yzb.nwpu.edu.cn/new/sszs/zsjz.htm"
+    else:
+        return []
+    res = requests.get(url)
+    res.encoding = 'utf-8'
+    html= res.text
+    soup = BeautifulSoup(html,"html.parser")
+    tags = soup.find(attrs={"class":'col-md-9 col-sm-9 col-xs-12 cno-right'})
+    list = tags.find_all(name = "li")
+    for i in range(len(list)):
+        list[i] = str(list[i])
+    return list
+
+def get_xgd_msg(index):
+    i= 1
+    msg_list = []
+    get_list = get_xgd(index)
+    for msg in get_list:
+        temp_msg = BeautifulSoup(msg, "html.parser")
+        title = str(temp_msg.a.string)
+        date = str(temp_msg.span.string)
+        href = "http://yzb.nwpu.edu.cn" + str(temp_msg.a["href"])[5:]
+        # print(msg)
+        # print(str(msg.a["href"]))
+
+        push_msg =  "序号："+ str(i)+"\n" \
+                    "日期：" + date + "\n" \
+                    "标题：" + title + \
+                    "链接：" + href + "\n"
+        msg_list.append(push_msg)
+        i=i+1
+    return msg_list
+
+def push_xgd():
+    i = 1
+    global xgd_last1,xgd_last2
+    xgd_latest1 = get_xgd(1)
+    xgd_latest2 = get_xgd(2)
+    #招生公告
+    new_msg1 = list(set(xgd_latest1).difference(set(xgd_last1))) #需要被推送更新的
+    old_msg1 = list(set(xgd_last1).difference(set(xgd_latest1))) #需要被淘汰的信息
+    #招生简章
+    new_msg2 = list(set(xgd_latest2).difference(set(xgd_last2))) #需要被推送更新的
+    old_msg2 = list(set(xgd_last2).difference(set(xgd_latest2))) #需要被淘汰的信息
+    push_msg = ""
+    if(new_msg1 != []):
+        push_msg =push_msg + "您有新的招生公告及招生简章信息：\n"
+        for msg in new_msg1:
+            temp_msg = BeautifulSoup(msg, "html.parser")
+            title = str(temp_msg.a.string)
+            date = str(temp_msg.span.string)
+            href = "http://yzb.nwpu.edu.cn" + str(temp_msg.a["href"])[5:]
+
+            push_msg =  push_msg+\
+                        "序号："+str(i)+"\n" \
+                        "日期："+date+"\n" \
+                        "标题："+title+ \
+                        "链接："+href+"\n\n"
+            i=i+1
+            # print(push_msg)
+        xgd_last1 = list((set(xgd_last1)-set(old_msg1))|set(new_msg1))
+
+    if (new_msg2 != []):
+        for msg in new_msg2:
+            temp_msg = BeautifulSoup(msg, "html.parser")
+            title = str(temp_msg.a.string)
+            date = str(temp_msg.span.string)
+            href = "http://yzb.nwpu.edu.cn" + str(temp_msg.a["href"])[5:]
+
+            push_msg = push_msg + \
+                       "序号：" + str(i) + "\n" \
+                        "日期：" + date + "\n" \
+                        "标题：" + title + \
+                        "链接：" + href + "\n\n"
+            i = i + 1
+            # print(push_msg)
+        xgd_last2 = list((set(xgd_last2) - set(old_msg2)) | set(new_msg2))
+    return push_msg
+
+def xgd_daily_push():
+    pushlist  = push_xgd()
+    if(pushlist!=[]):
+        for people in push_list:
+            user_name = itchat.search_friends(remarkName=people["remark_name"])[0]["UserName"]
+            itchat.send_msg(pushlist, toUserName=user_name)
+
 
 #图灵机器人
 def get_response(msg):
@@ -131,7 +357,7 @@ def saylove():
 
 #定时消息
 def clock_msg():
-
+    i = 0
     while 1:
         now = datetime.datetime.now()
         now_str = now.strftime('%Y/%m/%d %H:%M:%S'[9:])
@@ -180,8 +406,19 @@ def daily_push():
 
 if __name__=="__main__":
     #研招网资讯推送列表
-    xjd_last = get_xjd()
-
+    try:
+        xjd_last  = get_xjd()
+        xgd_last1 = get_xgd(1)
+        xgd_last2 = get_xgd(2)
+    except:
+        pass
+    # 艾宾浩斯任务盒子
+    with open("msg.txt", "r+") as f:
+        textcontent =  str(f.read())
+        if(textcontent!=""):
+            msgbox = decode(textcontent)
+        else:
+            msgbox = []
     #状态
     state = 0
     # 定时事件
@@ -192,6 +429,27 @@ if __name__=="__main__":
         "time": "09:00:00",
         "function": daily_push,
         },
+        {
+        "name":"xjd_daily_push",
+        "id":1,
+        "time":"12:00:00",
+        "function":xjd_daily_push,
+        },
+        {
+        "name": "xgd_daily_push",
+        "id": 2,
+        "time": "12:00:00",
+        "function": xgd_daily_push,
+
+        },
+        {
+        "name": "msg_daily_push",
+        "id": 3,
+        "time": "08:00:00",
+        "function": msg_daily_push,
+        }
+
+
     ]
     #推送列表
     push_list=[
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
-# import requests
-# import json
+import requests
+import json
+from bs4 import BeautifulSoup
 # url = "http://aider.meizu.com/app/weather/listWeather?cityIds=101270101"
 # print(url)
 # response = requests.get(url)
@@ -47,40 +48,203 @@
 #     pass
 
 
-import requests
-from bs4 import BeautifulSoup
 
-def get_xjd():
-    url = "http://yz.xjtu.edu.cn/index/zsdt.htm"
-    res = requests.get(url)
-    res.encoding = 'utf-8'
-    html= res.text
-    # print(html)
-    soup = BeautifulSoup(html,"html.parser")
-    list = soup.findAll(name="li", attrs={"class" :"clearfix"})
-    return list
-
-def get_xgd():
-    url = "http://yzb.nwpu.edu.cn/new/sszs/zsgg.htm"
-    res = requests.get(url)
-    res.encoding = 'utf-8'
-    html= res.text
-    soup = BeautifulSoup(html,"html.parser")
-    tags = soup.find(attrs={"class":'col-md-9 col-sm-9 col-xs-12 cno-right'})
-    list1 = tags.find_all(name = "li")
-
-    url = "http://yzb.nwpu.edu.cn/new/sszs/zsjz.htm"
-    res = requests.get(url)
-    res.encoding = 'utf-8'
-    html = res.text
-    soup = BeautifulSoup(html, "html.parser")
-    tags = soup.find(attrs={"class": 'col-md-9 col-sm-9 col-xs-12 cno-right'})
-    list2 = tags.find_all(name="li")
-    print(list2)
+# def get_xjd():
+#     url = "http://yz.xjtu.edu.cn/index/zsdt.htm"
+#     res = requests.get(url)
+#     res.encoding = 'utf-8'
+#     html= res.text
+#     # print(html)
+#     soup = BeautifulSoup(html,"html.parser")
+#     list = soup.findAll(name="li", attrs={"class" :"clearfix"})
+#     for i in range(len(list)):
+#         list[i] = str(list[i])
+#     # global xjd_last
+#     # xjd_last = list
+#     return list
 
 
-a = [1,2,3]
-b = [1,2,4]
-print(list(set(b).difference(set(a))))
+# xjd_last = get_xjd()
+#
+# xjd_last[0] = str('<li class="clearfix" id="line_u7_0">\n' \
+#               '<a href="../info/1082/3104.htm" target="_blank" title="西安通大学2021年硕士研究生招生复试录取工作方案">\n' \
+#               '<span class="date fr">2021-03-19</span>西安交通大学2021年硕士研究生招生复试录取工作方案</a>\n' \
+#               '</li>')
+# print(get_xjd()[0] == xjd_last[0])
 
 
+
+# def push_xjd():
+#     global xjd_last
+#     xjd_latest = get_xjd()
+#     new_msg = list(set(xjd_latest).difference(set(xjd_last))) #需要被推送更新的
+#     old_msg = list(set(xjd_last).difference(set(xjd_latest))) #需要被淘汰的信息
+#
+#     if(new_msg != []):
+#         for msg in new_msg:
+#             temp_msg = BeautifulSoup(msg,"html.parser").li.a
+#             title = str(temp_msg['title'])
+#             date = str(temp_msg.span.string)
+#             href = "http://yz.xjtu.edu.cn/"+str(temp_msg['href'])[3:]
+#
+#             push_msg =  "您有一条新的院校信息：\n" \
+#                         "日期："+date+"\n" \
+#                         "标题："+title+"\n" \
+#                         "链接："+href+"\n"
+#             print(push_msg)
+#         xjd_last = list((set(xjd_last)-set(old_msg))|set(new_msg))
+
+# def get_xjd_msg():
+#     i= 1
+#     msg_list = []
+#     get_list = get_xjd()
+#     for msg in get_list:
+#         temp_msg = BeautifulSoup(msg, "html.parser").li.a
+#         title = str(temp_msg['title'])
+#         date = str(temp_msg.span.string)
+#         href = "http://yz.xjtu.edu.cn/" + str(temp_msg['href'])[3:]
+#         # print(href)
+#
+#         push_msg =  "序号："+ str(i)+"\n" \
+#                     "日期：" + date + "\n" \
+#                     "标题：" + title + "\n" \
+#                     "链接：" + href + "\n"
+#         msg_list.append(push_msg)
+#         i=i+1
+#     return msg_list
+# def get_xgd(index):
+#     if(index == 1): #招生公告
+#         url = "http://yzb.nwpu.edu.cn/new/sszs/zsgg.htm"
+#     elif(index == 2): #招生简章
+#         url = "http://yzb.nwpu.edu.cn/new/sszs/zsjz.htm"
+#     else:
+#         return []
+#     res = requests.get(url)
+#     res.encoding = 'utf-8'
+#     html= res.text
+#     soup = BeautifulSoup(html,"html.parser")
+#     tags = soup.find(attrs={"class":'col-md-9 col-sm-9 col-xs-12 cno-right'})
+#     list = tags.find_all(name = "li")
+#     for i in range(len(list)):
+#         list[i] = str(list[i])
+#     return list
+#
+# def get_xgd_msg(index):
+#     i= 1
+#     msg_list = []
+#     get_list = get_xgd(index)
+#     for msg in get_list:
+#         temp_msg = BeautifulSoup(msg, "html.parser")
+#         title = str(temp_msg.a.string)
+#         date = str(temp_msg.span.string)
+#         href = "http://yzb.nwpu.edu.cn" + str(temp_msg.a["href"])[5:]
+#         # print(msg)
+#         # print(str(msg.a["href"]))
+#
+#         push_msg =  "序号："+ str(i)+"\n" \
+#                     "日期：" + date + "\n" \
+#                     "标题：" + title + "\n" \
+#                     "链接：" + href + "\n"
+#         msg_list.append(push_msg)
+#         i=i+1
+#     return msg_list
+#
+# def push_xgd():
+#     i = 1
+#     global xgd_last1,xgd_last2
+#     xgd_latest1 = get_xgd(1)
+#     xgd_latest2 = get_xgd(2)
+#     #招生公告
+#     new_msg1 = list(set(xgd_latest1).difference(set(xgd_last1))) #需要被推送更新的
+#     old_msg1 = list(set(xgd_last1).difference(set(xgd_latest1))) #需要被淘汰的信息
+#     #招生简章
+#     new_msg2 = list(set(xgd_latest2).difference(set(xgd_last2))) #需要被推送更新的
+#     old_msg2 = list(set(xgd_last2).difference(set(xgd_latest2))) #需要被淘汰的信息
+#     push_msg = ""
+#     if(new_msg1 != []):
+#         push_msg =push_msg + "您有新的招生公告及招生简章信息：\n"
+#         for msg in new_msg1:
+#             temp_msg = BeautifulSoup(msg, "html.parser")
+#             title = str(temp_msg.a.string)
+#             date = str(temp_msg.span.string)
+#             href = "http://yzb.nwpu.edu.cn" + str(temp_msg.a["href"])[5:]
+#
+#             push_msg =  push_msg+\
+#                         "序号："+str(i)+"\n" \
+#                         "日期："+date+"\n" \
+#                         "标题："+title+ \
+#                         "链接："+href+"\n\n"
+#             i=i+1
+#             # print(push_msg)
+#         xgd_last1 = list((set(xgd_last1)-set(old_msg1))|set(new_msg1))
+#
+#     if (new_msg2 != []):
+#         for msg in new_msg2:
+#             temp_msg = BeautifulSoup(msg, "html.parser")
+#             title = str(temp_msg.a.string)
+#             date = str(temp_msg.span.string)
+#             href = "http://yzb.nwpu.edu.cn" + str(temp_msg.a["href"])[5:]
+#
+#             push_msg = push_msg + \
+#                        "序号：" + str(i) + "\n" \
+#                         "日期：" + date + "\n" \
+#                         "标题：" + title + \
+#                         "链接：" + href + "\n\n"
+#             i = i + 1
+#             # print(push_msg)
+#         xgd_last2 = list((set(xgd_last2) - set(old_msg2)) | set(new_msg2))
+#     return push_msg
+#
+# xgd_last1 =  get_xgd(1)
+# xgd_last2 =  get_xgd(2)
+# print(xgd_last2)
+# xgd_last1[0] = str('<li><span>2021-07-01</span><a href="../../info/1174/7905.htm">关于发放2021级硕士研究生录取通知书的通知\r\n</a> </li>')
+# xgd_last2[0] = str('<li><span>2020-10-20</span><a href="../../info/1175/7655.htm">  2021年单独命题入学考试招生简章 \r\n</a> </li>')
+# print(push_xgd())
+import json
+# from demjson import *
+# # msgbox = []
+# #
+# # def creatmsg(msg,nickname):
+# #     global msgbox
+# #     m = {
+# #         "msg":msg,
+# #         "nickname":nickname,
+# #         "day":0
+# #     }
+# #     msgbox.append(m)
+# #     with open("msg.txt","w+") as f:
+# #         f.write(encode(msgbox))
+# #
+# # def msg_daily_push():
+# #     with open("msg.txt", "r+") as f:
+# #         temp_msg = decode(f.read())
+# #         for msg in temp_msg:
+# #             print(msg)
+# #             msg["day"]=msg["day"]+1
+# #             if(msg["day"]==1):
+# #                 print(1)
+# #             elif(msg["day"]==2):
+# #                 print(2)
+# #             elif (msg["day"] == 3):
+# #                 print(3)
+# #             elif (msg["day"] == 4):
+# #                 print(4)
+# #             elif (msg["day"] == 5):
+# #                 temp_msg.remove(msg)
+# #         with open("msg.txt", "w+") as f:
+# #             f.write(encode(temp_msg))
+# #         global msgbox
+# #         msgbox = temp_msg
+# #         print(temp_msg)
+# # creatmsg("szw","1314")
+# # msg_daily_push()
+# # creatmsg("lyt","1314")
+# # msg_daily_push()
+# # msg_daily_push()
+# # msg_daily_push()
+# # msg_daily_push()
+from demjson import *
+a = "{'MsgId': '1297252672273560629', 'FromUserName': '@717c84132424761a3e13775f3ed8fa12b223b5df7c33fe9d3c9299e0d5f3b78d', 'ToUserName': '@ed82706fa3b1642bf1d905d325efac7cbed2ff7910b02e39bf0b05b1b0334626', 'MsgType': 1, 'Content': '。', 'Status': 3, 'ImgStatus': 1, 'CreateTime': 1629529861, 'VoiceLength': 0, 'PlayLength': 0, 'FileName': '', 'FileSize': '', 'MediaId': '', 'Url': '', 'AppMsgType': 0, 'StatusNotifyCode': 0, 'StatusNotifyUserName': '', 'RecommendInfo': {'UserName': '', 'NickName': '', 'QQNum': 0, 'Province': '', 'City': '', 'Content': '', 'Signature': '', 'Alias': '', 'Scene': 0, 'VerifyFlag': 0, 'AttrStatus': 0, 'Sex': 0, 'Ticket': '', 'OpCode': 0}, 'ForwardFlag': 0, 'AppInfo': {'AppID': '', 'Type': 0}, 'HasProductId': 0, 'Ticket': '', 'ImgHeight': 0, 'ImgWidth': 0, 'SubMsgType': 0, 'NewMsgId': 1297252672273560629, 'OriContent': '', 'EncryFileName': '', 'User': <User: {'MemberList': <ContactList: []>, 'Uin': 0, 'UserName': '@717c84132424761a3e13775f3ed8fa12b223b5df7c33fe9d3c9299e0d5f3b78d', 'NickName': 'shallwe', 'HeadImgUrl': '/cgi-bin/mmwebwx-bin/webwxgeticon?seq=709550538&username=@717c84132424761a3e13775f3ed8fa12b223b5df7c33fe9d3c9299e0d5f3b78d&skey=', 'ContactFlag': 1, 'MemberCount': 0, 'RemarkName': 'szw', 'HideInputBarFlag': 0, 'Sex': 1, 'Signature': '', 'VerifyFlag': 0, 'OwnerUin': 0, 'PYInitial': 'SHALLWE', 'PYQuanPin': 'shallwe', 'RemarkPYInitial': 'SZW', 'RemarkPYQuanPin': 'szw', 'StarFriend': 0, 'AppAccountFlag': 0, 'Statues': 0, 'AttrStatus': 233765, 'Province': '陕西', 'City': '汉中', 'Alias': '', 'SnsFlag': 0, 'UniFriend': 0, 'DisplayName': '', 'ChatRoomId': 0, 'KeyWord': '', 'EncryChatRoomId': '', 'IsOwner': 0}>, 'Type': 'Text', 'Text': '。'}"
+
+print(decode(a.replace("<","{").replace(">","}"))['User']['User']['RemarkName'])
